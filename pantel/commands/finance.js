537d276796580c73e1906fa827f9f226b8fefab8
danielbauman1010 <danielbauman1010@gmail.com>
2018-08-23 17:33:06 +0300

    Finance works, you can set a balance and show it

--- pantel/commands/finance.js
+++ pantel/commands/finance.js
@@ -1,57 +1,67 @@
 const control = require('../controlFunctions');
 
+var perm = "";
+function setup(){
+  control.addCollection("finance").then(function(permN){
+    perm = permN;
+  },function(err){
+    console.log(err);
+  });
+}
+
 exports.commands = {
   'set balance': function(user) {
     return new Promise(function(resolve, reject) {
       control.addListener(user, function(user,command){
         return new Promise(function(resolve, reject) {
-          control.removeListener(user).then(function(result){
             const balance = Number(command);
             if(isNaN(balance)){
               resolve("Sorry, but you must enter a number.");
             } else {
-              control.findInCollection("finance", {"username": user.username}).then(function(results){
+              let query = {};
+              query['username'] = user.username
+              control.findInCollection("finance", perm, query).then(function(results){
                 if(results.length>0){
-
+                  let dataBlock = {};
+                  dataBlock['balance'] = balance;
+                  let newvals = {};
+                  newvals['$set'] = dataBlock;
+                  control.update('finance', perm, query, newvals);
+                  resolve('Balance set to '+balance);
                 } else {
                   let userDoc = {};
                   userDoc.username = user.username;
                   userDoc["balance"] = balance;
                   userDoc["purchase history"] = {};
                   userDoc["income histroy"] = {};
-                  addToCollection("finance", userDoc).then(function(results){
-                    resolve('Balance set, account created.');
-                  },function(err){
-                    reject(err);
-                  });
+                  control.addToCollection("finance", perm, userDoc);
+                  resolve('Balance set, account created.');
                 }
               },function(err){
                 console.log(err);
                 reject(err);
               });
             }
-          },function(err){
-            reject(err);
-          })
-        });
-      }).then(function(result){
-        resolve("What is your balance? Please enter a number without a currency, symbols, or letters.");
-      },function(err){
-        reject(err);
+          });
       });
+      resolve("What is your balance? Please enter a number without a currency, symbols, or letters.");
     });
   },
   "show balance": function(user){
     return new Promise(function(resolve, reject) {
-      if('finance' in user.data){
-        if('balance' in user.data.finance){
-          resolve("Balance: "+user.data.finance.balance);
+      let query = {};
+      query['username'] = user.username;
+      control.findInCollection('finance', perm, query).then(function(results){
+        if(results.length==0){
+          resolve("You have no balance set.");
         } else {
-          resolve("No balance set. Set balance by typing \"set balance\".");
+          let account = results[0];
+          resolve(account['balance']);
         }
-      } else {
-        resolve("No financial record set. Set balance by typing \"set balance\".");
-      }
+      },function(err){
+        reject(err);
+      });
     });
   }
 }
+exports.setup = setup;
